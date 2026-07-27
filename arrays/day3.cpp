8b43d2fc1f6e4097541adacd94078eb2e6c0e37d
Vennela <vennelareddyvari.11@gmail.com>
2026-07-27 21:19:47 +0530

    DAY 3: Remove Element (easy)

---
+++ arrays/day3.cpp
@@ -0,0 +1,22 @@
+#removeElement
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0, j = 0;
+        while (i < nums.size())
+        {
+            if (nums[i] != val)
+            {
+                nums[j] = nums[i];
+                j++;
+                i++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return j;
+    }
+};
