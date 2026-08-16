22d53fe81c3451035ee775e7f5d4c48394027261
Vennela <vennelareddyvari.11@gmail.com>
2026-08-16 10:55:19 +0530

    Day 13: Find indices of Stable Mountains (easy)

---
+++ arrays/day13.cpp
@@ -0,0 +1,16 @@
+#findIndicesOfStableMountains
+
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        vector<int> indices;
+        for (int i = 1; i < height.size(); i++)
+        {
+            if (height[i - 1] > threshold)
+            {
+                indices.push_back(i);
+            }
+        }
+        return indices;
+    }
+};
