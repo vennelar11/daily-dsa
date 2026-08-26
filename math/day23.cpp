461b0abb40986a0cbb9f81f2ae6ee194929daa59
Vennela <vennelareddyvari.11@gmail.com>
2026-08-26 19:09:50 +0530

    Day 23: Valid Perfect Square (easy)

---
+++ math/day23.cpp
@@ -0,0 +1,15 @@
+#validPerfectSquare
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for (int i = 1; i <= num / i; i++)
+        {
+            if (i * i == num)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
