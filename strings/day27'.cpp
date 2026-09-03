ad178b6c35e49c00334335aeb2959bd08a3b4520
Vennela <vennelareddyvari.11@gmail.com>
2026-09-03 19:40:18 +0530

    Day 27: Reverse String (easy)

---
+++ strings/day27'.cpp
@@ -0,0 +1,13 @@
+#reverseString
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        char temp;
+        int n = s.size();
+        for (int i = 0; i < n / 2; i++)
+        {
+            swap(s[i], s[n - 1 - i]);
+        }
+    }
+};
