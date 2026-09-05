c651f1d1164cd81b0f6b18a117b24bb14c9cbc6c
Vennela <vennelareddyvari.11@gmail.com>
2026-09-05 22:54:38 +0530

    Day 31: Single Number (easy)

---
+++ arrays/day31.cpp
@@ -0,0 +1,20 @@
+#singleNumber
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if (nums.size() == 1)
+        {
+            return nums[0];
+        }
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i += 2)
+        {
+            if (nums[i] != nums[i + 1])
+            {
+                return nums[i];
+            }
+        }
+        return nums[nums.size() - 1];
+    }
+};
