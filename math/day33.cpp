2e1e1729cd0fd40874a1b5048b5e89506bd169b8
Vennela <vennelareddyvari.11@gmail.com>
2026-09-08 22:20:33 +0530

    Day 33: Count Commas In Range (easy)

---
+++ math/day33.cpp
@@ -0,0 +1,13 @@
+#countCommasInRange
+
+class Solution {
+public:
+    int countCommas(int n) 
+    {
+        if (n < 1000)
+        {
+            return 0;
+        }
+        return n - 1000 + 1;
+    }
+};
