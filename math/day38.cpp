ff2a1c3f17ff59425426ae4e356b93b998c4454e
Vennela <vennelareddyvari.11@gmail.com>
2026-09-12 22:18:01 +0530

    Day 38: Number of Common Factors (easy)

---
+++ math/day38.cpp
@@ -0,0 +1,16 @@
+#numberOfCommonFactors
+
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int count = 0;
+        for (int i = 1; i <= max(a, b); i++)
+        {
+            if (a % i == 0 && b % i == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
