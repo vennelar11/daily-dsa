850347829b813599665d6ca2e9c7abc5c8c9998e
Vennela <vennelareddyvari.11@gmail.com>
2026-09-14 11:11:39 +0530

    Day 39: Counting words with a given prefix (easy)

---
+++ strings/day39.cpp
@@ -0,0 +1,16 @@
+#countingWordsWithGivenPrefix
+
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int n = pref.length(), count = 0;
+        for (int i = 0; i < words.size(); i++)
+        {
+            if (words[i].substr(0, n) == pref)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
