a1285cd8e1534b9f70896615a68d7b345baf2984
Vennela <vennelareddyvari.11@gmail.com>
2026-09-25 18:09:38 +0530

    Day 49: Count Operations to obtain zero (easy)

---
+++ math/day49.cpp
@@ -0,0 +1,21 @@
+#countOperationsToObtainZero
+
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 != 0 && num2 != 0)
+        {
+            if (num1 >= num2)
+            {
+                num1 -= num2;
+            }
+            else if (num2 >= num1)
+            {
+                num2 -= num1;
+            }
+            count++;
+        }
+        return count;
+    }
+};
